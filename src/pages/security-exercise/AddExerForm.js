a5fa493313cb8d72aba972832a2000365a044ff4
joy45689 <joy45689acc@gmail.com>
2024-01-23 16:14:00 +0800

    Update "Add Security Exercise" form

--- src/pages/security-exercise/AddExerForm.js
+++ src/pages/security-exercise/AddExerForm.js
@@ -1,21 +1,13 @@
-import { useEffect, useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
 
 // material-ui
 import {
-  Box,
   Button,
-  Divider,
-  FormControl,
   FormHelperText,
   Grid,
-  Link,
-  IconButton,
-  InputAdornment,
   InputLabel,
   OutlinedInput,
-  Stack,
-  Typography
+  Stack
 } from '@mui/material';
 
 // third party
@@ -23,56 +15,50 @@ import * as Yup from 'yup';
 import { Formik } from 'formik';
 
 // project import
-import FirebaseSocial from './FirebaseSocial';
 import AnimateButton from 'components/@extended/AnimateButton';
-import { strengthColor, strengthIndicator } from 'utils/password-strength';
 
 // assets
-import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 
-// ============================|| FIREBASE - REGISTER ||============================ //
+// ============================|| FIREBASE - ADD SECURITY EXERCISE ||============================ //
+function formatDate(){
+  let date = new Date();
+
+  let day = date.getDate();
+  let month = date.getMonth()+1;
+  let year = date.getFullYear();
+
+  if (day < 10) {
+      day = '0' + day;
+  }
+  if (month < 10) {
+      month = `0${month}`;
+  }
+  
+  // return year + "-" + month + "-" + day;
+  return `${year}-${month}-${day}`;
+}
 
 const AddExerForm = () => {
-  const [level, setLevel] = useState();
-  const [showPassword, setShowPassword] = useState(false);
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  const changePassword = (value) => {
-    const temp = strengthIndicator(value);
-    setLevel(strengthColor(temp));
-  };
-
-  useEffect(() => {
-    changePassword('');
-  }, []);
 
   return (
     <>
       <Formik
         initialValues={{
-          firstname: '',
-          lastname: '',
-          email: '',
-          company: '',
-          password: '',
+          organization: '',
+          start: formatDate(),
+          end: formatDate(),
           submit: null
         }}
         validationSchema={Yup.object().shape({
-          firstname: Yup.string().max(255).required('First Name is required'),
-          lastname: Yup.string().max(255).required('Last Name is required'),
-          email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-          password: Yup.string().max(255).required('Password is required')
+          organization: Yup.string().max(255).required('Organization is required'),
+          start: Yup.string().max(255).required('Start is required'),
+          end: Yup.string().max(255).required('End is required')
         })}
         onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
           try {
             setStatus({ success: false });
-            setSubmitting(false);
+            setSubmitting(false);            
+            console.log(values);
           } catch (err) {
             console.error(err);
             setStatus({ success: false });
@@ -84,154 +70,75 @@ const AddExerForm = () => {
         {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
           <form noValidate onSubmit={handleSubmit}>
             <Grid container spacing={3}>
+              {/* Organization */}
+              <Grid item xs={12}>
+                <Stack spacing={1}>
+                  <InputLabel htmlFor="organization-add-exer">Organization*</InputLabel>
+                  <OutlinedInput
+                    id="organization-add-exer"
+                    type="text"
+                    value={values.organization}
+                    name="organization"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    placeholder="delta"
+                    fullWidth
+                    error={Boolean(touched.organization && errors.organization)}
+                  />
+                  {touched.organization && errors.organization && (
+                    <FormHelperText error id="helper-text-organization-add-exer">
+                      {errors.organization}
+                    </FormHelperText>
+                  )}
+                </Stack>
+              </Grid>
+
+              {/* Start */}
               <Grid item xs={12} md={6}>
                 <Stack spacing={1}>
-                  <InputLabel htmlFor="firstname-signup">First Name*</InputLabel>
+                  <InputLabel htmlFor="start-add-exer">Start*</InputLabel>
                   <OutlinedInput
-                    id="firstname-login"
-                    type="firstname"
-                    value={values.firstname}
-                    name="firstname"
+                    fullWidth
+                    error={Boolean(touched.start && errors.start)}
+                    id="start-add-exer"
+                    type="date"
+                    value={values.start}
+                    name="start"
                     onBlur={handleBlur}
                     onChange={handleChange}
-                    placeholder="John"
-                    fullWidth
-                    error={Boolean(touched.firstname && errors.firstname)}
+                    inputProps={{}}
                   />
-                  {touched.firstname && errors.firstname && (
-                    <FormHelperText error id="helper-text-firstname-signup">
-                      {errors.firstname}
+                  {touched.start && errors.start && (
+                    <FormHelperText error id="helper-text-start-add-exer">
+                      {errors.start}
                     </FormHelperText>
                   )}
                 </Stack>
               </Grid>
+
+              {/* End */}
               <Grid item xs={12} md={6}>
                 <Stack spacing={1}>
-                  <InputLabel htmlFor="lastname-signup">Last Name*</InputLabel>
+                  <InputLabel htmlFor="end-add-exer">End*</InputLabel>
                   <OutlinedInput
                     fullWidth
-                    error={Boolean(touched.lastname && errors.lastname)}
-                    id="lastname-signup"
-                    type="lastname"
-                    value={values.lastname}
-                    name="lastname"
+                    error={Boolean(touched.end && errors.end)}
+                    id="end-add-exer"
+                    type="date"
+                    value={values.end}
+                    name="end"
                     onBlur={handleBlur}
                     onChange={handleChange}
-                    placeholder="Doe"
                     inputProps={{}}
                   />
-                  {touched.lastname && errors.lastname && (
-                    <FormHelperText error id="helper-text-lastname-signup">
-                      {errors.lastname}
+                  {touched.end && errors.end && (
+                    <FormHelperText error id="helper-text-end-add-exer">
+                      {errors.end}
                     </FormHelperText>
                   )}
                 </Stack>
               </Grid>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="company-signup">Company</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.company && errors.company)}
-                    id="company-signup"
-                    value={values.company}
-                    name="company"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="Demo Inc."
-                    inputProps={{}}
-                  />
-                  {touched.company && errors.company && (
-                    <FormHelperText error id="helper-text-company-signup">
-                      {errors.company}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="email-signup">Email Address*</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.email && errors.email)}
-                    id="email-login"
-                    type="email"
-                    value={values.email}
-                    name="email"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="demo@company.com"
-                    inputProps={{}}
-                  />
-                  {touched.email && errors.email && (
-                    <FormHelperText error id="helper-text-email-signup">
-                      {errors.email}
-                    </FormHelperText>
-                  )}
-                </Stack>
-              </Grid>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="password-signup">Password</InputLabel>
-                  <OutlinedInput
-                    fullWidth
-                    error={Boolean(touched.password && errors.password)}
-                    id="password-signup"
-                    type={showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    name="password"
-                    onBlur={handleBlur}
-                    onChange={(e) => {
-                      handleChange(e);
-                      changePassword(e.target.value);
-                    }}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                          size="large"
-                        >
-                          {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                    placeholder="******"
-                    inputProps={{}}
-                  />
-                  {touched.password && errors.password && (
-                    <FormHelperText error id="helper-text-password-signup">
-                      {errors.password}
-                    </FormHelperText>
-                  )}
-                </Stack>
-                <FormControl fullWidth sx={{ mt: 2 }}>
-                  <Grid container spacing={2} alignItems="center">
-                    <Grid item>
-                      <Box sx={{ bgcolor: level?.color, width: 85, height: 8, borderRadius: '7px' }} />
-                    </Grid>
-                    <Grid item>
-                      <Typography variant="subtitle1" fontSize="0.75rem">
-                        {level?.label}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="body2">
-                  By Signing up, you agree to our &nbsp;
-                  <Link variant="subtitle2" component={RouterLink} to="#">
-                    Terms of Service
-                  </Link>
-                  &nbsp; and &nbsp;
-                  <Link variant="subtitle2" component={RouterLink} to="#">
-                    Privacy Policy
-                  </Link>
-                </Typography>
-              </Grid>
+
               {errors.submit && (
                 <Grid item xs={12}>
                   <FormHelperText error>{errors.submit}</FormHelperText>
@@ -240,17 +147,9 @@ const AddExerForm = () => {
               <Grid item xs={12}>
                 <AnimateButton>
                   <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
-                    Create Account
+                    Create
                   </Button>
                 </AnimateButton>
-              </Grid>
-              <Grid item xs={12}>
-                <Divider>
-                  <Typography variant="caption">Sign up with</Typography>
-                </Divider>
-              </Grid>
-              <Grid item xs={12}>
-                <FirebaseSocial />
               </Grid>
             </Grid>
           </form>
--- src/pages/security-exercise/index.js
+++ src/pages/security-exercise/index.js
@@ -17,6 +17,8 @@ import {
   // TextField,
   Typography
 } from '@mui/material';
+// import { styled } from '@mui/material/styles';
+// import Paper from '@mui/material/Paper';
 
 // project import
 import ExerciseTable from 'pages/security-exercise/ExerciseTable';
@@ -24,7 +26,7 @@ import AddExerForm from './AddExerForm';
 import MainCard from 'components/MainCard';
 
 // assets
-// import { GiftOutlined, MessageOutlined, SettingOutlined } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 // import avatar1 from 'assets/images/users/avatar-1.png';
 // import avatar2 from 'assets/images/users/avatar-2.png';
 // import avatar3 from 'assets/images/users/avatar-3.png';
@@ -65,31 +67,55 @@ import MainCard from 'components/MainCard';
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
 
 const SecurityExercise = () => {
   const [showAddExerForm, setShowAddExerForm] = useState(false);
   const addExercise = () => {
     setShowAddExerForm(!showAddExerForm);
-  }
+  };
 
   return (
-    <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-      {/* row 0 */}
+    <Grid container rowSpacing={3} columnSpacing={2.75}>
+
+      {/* Title */}
       <Grid item xs={12} md={7} lg={8}>
         <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
-            <Typography variant="h5">Recent Secury Exercises</Typography>
-          </Grid>
-          <Grid item />
+          <Typography variant="h5">Secury Exercises</Typography>
         </Grid>
-        <Button onClick={addExercise} variant="contained" color="success" size="small">
-          Create a new Activity
-        </Button>
-        {showAddExerForm && <AddExerForm />}
-        <MainCard sx={{ mt: 2 }} content={false}>
+      </Grid>
+
+
+      {/* Buttons */}
+      <Grid item xs={12}>
+        <Grid container rowSpacing={1}>
+          <Grid item xs={12}>
+            <Button onClick={addExercise} variant="contained" color="success" size="small" startIcon={<PlusOutlined />}>
+              Add
+            </Button>
+          </Grid>
+          {/* Form */}
+          {
+            showAddExerForm && <Grid item xs={12} md={4} lg={4}>
+              <AddExerForm  />
+            </Grid>
+          }
+        </Grid>
+      </Grid>
+
+      {/* Tables */}
+      <Grid item xs={12} md={7} lg={8}>
+        <MainCard content={false}>
           <ExerciseTable />
         </MainCard>
       </Grid>
+
     </Grid>
   );
 };
