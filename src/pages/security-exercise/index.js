65069ae29bb5bdf2e652b12b30bb67c10a2101ec
joy45689 <joy45689acc@gmail.com>
2024-01-26 15:17:41 +0800

    Refresh the exercise table after adding successfully

--- src/pages/security-exercise/index.js
+++ src/pages/security-exercise/index.js
@@ -24,7 +24,7 @@ const SecurityExercise = () => {
   const [exerciseList, setExerciseList] = useState([]);
   useEffect(() => {
     loadExercises(setExerciseList);
-  }, []);
+  }, [showAddExerForm]);
 
   const addExercise = () => {
     console.log("showAddExerForm: "+ showAddExerForm)
