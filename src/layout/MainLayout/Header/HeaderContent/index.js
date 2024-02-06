d125f98f9b5aadb468861e06e16789bc9c2ea46a
joy45689 <joy45689acc@gmail.com>
2024-02-06 14:25:11 +0800

    [Update] Remove unused component in toolbar

--- src/layout/MainLayout/Header/HeaderContent/index.js
+++ src/layout/MainLayout/Header/HeaderContent/index.js
@@ -1,11 +1,12 @@
 // material-ui
-import { Box, IconButton, Link, useMediaQuery } from '@mui/material';
-import { GithubOutlined } from '@ant-design/icons';
+import {  Box, useMediaQuery } from '@mui/material';
+// import { Box, IconButton, Link, useMediaQuery } from '@mui/material';
+// import { GithubOutlined } from '@ant-design/icons';
 
 // project import
-import Search from './Search';
+// import Search from './Search';
 import Profile from './Profile';
-import Notification from './Notification';
+// import Notification from './Notification';
 import MobileSection from './MobileSection';
 
 // ==============================|| HEADER - CONTENT ||============================== //
@@ -15,10 +16,11 @@ const HeaderContent = () => {
 
   return (
     <>
-      {!matchesXs && <Search />}
-      {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />}
+      {/* {!matchesXs && <Search />} */}
+      {/* {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />} */}
+      {<Box sx={{ width: '100%', ml: 1 }} />}
 
-      <IconButton
+      {/* <IconButton
         component={Link}
         href="https://github.com/codedthemes/mantis-free-react-admin-template"
         target="_blank"
@@ -28,9 +30,9 @@ const HeaderContent = () => {
         sx={{ color: 'text.primary', bgcolor: 'grey.100' }}
       >
         <GithubOutlined />
-      </IconButton>
+      </IconButton> */}
 
-      <Notification />
+      {/* <Notification /> */}
       {!matchesXs && <Profile />}
       {matchesXs && <MobileSection />}
     </>
